082eece5c357fe94741f4ab76745657f6843e032
Anatoly Pozdnyshev <pozdnyshev.anatol@gmail.com>
2023-09-09 15:12:42 +0300

    Cor

--- practice_4/vova_win.py
+++ practice_4/vova_win.py
@@ -26,29 +26,30 @@ def result(x):
     return ''
 
 
-for key, value in RACE_DATA.items():
-    if key == winer(1):
-        for key_1, value_1 in value.items():
-            if key_1 == 'RacerName':
-                name_win = (f'Выиграл - {value_1}!!! Поздравляем!')
-                print(name_win)
-                print('_' * len(name_win))
-                print('Первые три места:')
-                print('Гонщик на 1 месте:')
-                print(f'\tИмя: {value_1}')
-            elif key_1 == 'RacerTeam':
-                print(f'\tКоманда: {value_1}')
-            elif key_1 == 'FinishedTimeSeconds':
-                minut = value_1 // 60
-                sec = value_1 % 60
-                hour = minut // 60
-                minut_1 = minut % 60
-                print(f'\t{hour:02}:{minut_1:02}:{sec:02}') 
-                print('')
 
-print(result(2))
-print(result(3))    
-print(winer(1)) 
+if __name__ == '__main__':
+    for key, value in RACE_DATA.items():
+        if key == winer(1):
+            for key_1, value_1 in value.items():
+                if key_1 == 'RacerName':
+                    name_win = (f'Выиграл - {value_1}!!! Поздравляем!')
+                    print(name_win)
+                    print('_' * len(name_win))
+                    print('Первые три места:')
+                    print('Гонщик на 1 месте:')
+                    print(f'\tИмя: {value_1}')
+                elif key_1 == 'RacerTeam':
+                    print(f'\tКоманда: {value_1}')
+                elif key_1 == 'FinishedTimeSeconds':
+                    minut = value_1 // 60
+                    sec = value_1 % 60
+                    hour = minut // 60
+                    minut_1 = minut % 60
+                    print(f'\t{hour:02}:{minut_1:02}:{sec:02}') 
+                    print('')
+    print(result(2))
+    print(result(3))    
+
 
 
 # print(winer(3))
